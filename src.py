41c18b1a2aaf1d71b4cb2b802feb06e83bf0f608
rheyhan <rheyhanfahry@apps.ipb.ac.id>
2026-01-21 18:10:46 +0700

    App test and output folder

--- src.py
+++ src.py
@@ -18,6 +18,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+print(F'Timezone: {time.strftime("%z", time.gmtime())}')
+
 
 lang_codes = {'Arabic': 'ar',
             'Arabic (Feminine)': 'ar-x-fm',
